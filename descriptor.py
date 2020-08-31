f6d4440d1a84926d4ff9c40be8503968071c53e6
Artur <aepremyan3993@gmail.com>
2020-08-31 15:09:27 +0300

    dfl;ld

---
+++ descriptor.py
@@ -0,0 +1,10 @@
+class Value:
+    def __init__(self):
+        self.value = None
+
+    def __get__(self, obj, obj_type):
+        return self.value
+
+    def __set__(self, obj, value):
+        self.value = int(value - value * obj.commission)
+
