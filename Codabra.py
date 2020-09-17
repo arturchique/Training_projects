309955fdb9d97873df59ed285dd86780f896c391
Artur <aepremyan3993@gmail.com>
2020-09-17 21:25:44 +0300

    Final Commit for game

--- Codabra.py
+++ Codabra.py
@@ -4,28 +4,9 @@ import sys
 import time
 
 
-def game_over():
-    go_font = pygame.font.SysFont("arial", 50)
-    go_surface = go_font.render(f"Вы проиграли со счетом: {score}", True, RED)
-    go_rect = go_surface.get_rect()
-    go_rect.midtop = (window_width / 2, window_height / 2 - 35)
-    game_display.fill(BLACK)
-    game_display.blit(go_surface, go_rect)
-    pygame.display.flip()
-    time.sleep(3)
-    pygame.quit()
-    sys.exit()
-
-
-def show_score():
-    score_font = pygame.font.SysFont("arial", 15)
-    score_surface = score_font.render(f"Счёт : {score}", True, WHITE)
-    score_rect = score_surface.get_rect()
-    game_display.blit(score_surface, score_rect)
-
-
 def change_direction():
-    global direction, change_to
+    """ Сверяем, можем ли мы изменить направление на заданное пользователем """
+    global direction, change_to # меняем значения переменных, существующих вне функции
     if change_to == 'UP' and direction != 'DOWN':
         direction = 'UP'
     if change_to == 'DOWN' and direction != 'UP':
@@ -37,6 +18,7 @@ def change_direction():
 
 
 def move_head():
+    """ Меняем координаты головы при движении в зависимости от направления """
     if direction == "RIGHT":
         snake_head[0] += 10
     elif direction == "LEFT":
@@ -48,16 +30,21 @@ def move_head():
 
 
 def draw_snake():
+    """ Вырисовываем все "блоки" змеи, предварительно заливая всю область
+    коричневым цветом, чтобы на каждоом кадре была актуальная змея """
     game_display.fill(BROWN)
     for pos in snake_body:
         pygame.draw.rect(game_display, GREEN, (pos[0], pos[1], 9, 9))
 
 
 def draw_food():
+    """ Вырисовываем еду """
     pygame.draw.rect(game_display, RED, (food[0], food[1], 9, 9))
 
 
 def check_for_crash():
+    """ Рассматриваем все условия, при которых игра закончится
+     и вызываем функцию game_over() """
     if snake_head[0] < 0 or snake_head[0] > window_width - 10 or snake_head[1] < 0 or snake_head[1] > window_height - 10:
         game_over()
     for body in snake_body[1:]:
@@ -66,53 +53,78 @@ def check_for_crash():
 
 
 def check_for_food():
-    global food, score
+    """ Проверка, не столкнулась ли змея с едой """
+    global food, score # работаем с переменными, существующими вне функции
     snake_body.insert(0, snake_head.copy())
     if snake_head[0] == food[0] and snake_head[1] == food[1]:
+        # если столкнулась с едой, то увеличим число очков и зададим новый координаты для еды
         score += 1
         food = [random.randrange(1, (window_width // 10)) * 10, random.randrange(1, (window_height // 10)) * 10]
     else:
+        # если не столкнулась, то удалим элемент с хвоста, так как змея будет двигаться
+        # за счет изменения координат головы
         snake_body.pop()
 
 
-# определяем цвета по их кодам
+def show_score():
+    """ Отрисовываем количество очков """
+    score_font = pygame.font.SysFont("arial", 15)
+    score_surface = score_font.render(f"Счёт : {score}", True, WHITE)
+    score_rect = score_surface.get_rect()
+    game_display.blit(score_surface, score_rect)
+
+
+def game_over():
+    """ Отрисовываем экран конца игры """
+    go_font = pygame.font.SysFont("arial", 50)
+    go_surface = go_font.render(f"Вы проиграли со счетом: {score}", True, RED)
+    go_rect = go_surface.get_rect()
+    go_rect.midtop = (window_width // 2, window_height // 2 - 35)
+    game_display.fill(BLACK)
+    game_display.blit(go_surface, go_rect)
+    pygame.display.update()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
+
+
+# Определяем цвета по их кодам
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BROWN = (59, 48, 36)
 
-# задаем параметры окна с игрой
+# Задаем параметры окна с игрой
 window_width = 800
 window_height = 600
 
-# создаем окно с игрой
+# Создаем окно с игрой
 pygame.init()
 game_display = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("The Snake")
 
-# задаем параметры будущей скорости обновления кадров
+# Задаем параметры будущей скорости обновления кадров
 clock = pygame.time.Clock()
-FPS = 10
+FPS = 25
 
-# параметры шрифта, которым будут писаться все
+# Параметры шрифта, которым будут писаться все
 font = pygame.font.SysFont(None, 25, bold=True)
 
-# начальные координаты и переменные для Змеи
+# Начальные координаты и переменные для Змеи
 snake_head = [100, 50]
 snake_body = [[100, 50], [90, 50], [80, 50]]
 direction = 'RIGHT'
 change_to = direction
 score = 0
 
-# начальные координаты для еды
+# Начальные координаты для еды
 food = [random.randrange(1, (window_width//10)) * 10, random.randrange(1, (window_height//10)) * 10]
 append_food = True
 
-exit_ = False
-
+# Далее -- основной цикл программы
 while True:
-    # обрабатываем все возможные события
+    # Обрабатываем все возможные события
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -127,21 +139,17 @@ while True:
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 change_to = "DOWN"
 
-    # изменяем направление змеи
+    # Изменяем направление змеи
     change_direction()
-
-    # двигаем голову змеи в соответствии с направлением
+    # Двигаем голову змеи в соответствии с направлением
     move_head()
-
-    # поведение змеи если она съела еду и если еда не была съедена
+    # Поведение змеи если она съела еду и если еда не была съедена
     check_for_food()
-
-    # отрисовка змеи
+    # Отрисовка змеи
     draw_snake()
-    # отрисовка еды
+    # Отрисовка еды
     draw_food()
-
-    # поведение змеи при аварии
+    # Поведение змеи при аварии
     check_for_crash()
 
     show_score()
